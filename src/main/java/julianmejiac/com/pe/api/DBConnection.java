e48fac88a5007369d8e0d269bd1a822653cab0e0
julianmejiac <julianmejiac@gmail.com>
2026-05-20 12:21:36 -0400

    Update DBConnection.java
    Eliminate unnecessary lines

--- src/main/java/julianmejiac/com/pe/api/DBConnection.java
+++ src/main/java/julianmejiac/com/pe/api/DBConnection.java
@@ -1,18 +1,14 @@
-/**
- * TODO:
- * Refactor database configuration to support Docker and multiple environments.
- * Current implementation relies on a hardcoded database URL.
- */
+
 package julianmejiac.com.pe.api;
 
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
-import java.io.InputStream;
+
 import java.sql.Connection;
-import java.sql.DriverManager;
+
 import java.sql.SQLException;
-import java.util.Properties;
+
 @Component
 
 public class DBConnection {
