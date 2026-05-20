1ba1dfd3c91e35400044426cb15e2c203040ff47
julianmejiac <julianmejiac@gmail.com>
2026-05-20 16:06:09 -0400

    Docker and Config
    Added FrontEnd Docker. Changed CORS port.

--- src/main/java/julianmejiac/com/pe/api/SecurityConfig.java
+++ src/main/java/julianmejiac/com/pe/api/SecurityConfig.java
@@ -40,7 +40,8 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:63342"));
+        config.setAllowedOrigins(List.of( "http://localhost:63342",
+                "http://localhost:3000"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
 
